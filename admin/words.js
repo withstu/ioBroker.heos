3f607a89514ed1b15b698f4bb81e0ff970d2e133
withstu <withstu@gmx.de>
2020-08-02 18:29:13 +0200

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'heos adapter settings': {
+		'en': 'Adapter settings for heos',
+		'de': 'Adaptereinstellungen für heos',
+		'ru': 'Настройки адаптера для heos',
+		'pt': 'Configurações do adaptador para heos',
+		'nl': 'Adapterinstellingen voor heos',
+		'fr': "Paramètres d'adaptateur pour heos",
+		'it': "Impostazioni dell'adattatore per heos",
+		'es': 'Ajustes del adaptador para heos',
+		'pl': 'Ustawienia adaptera dla heos',
+		'zh-cn': 'heos的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
